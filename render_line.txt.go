b3938843b7ff0b730080d00f13e0db8bfa3e1b1c
ejembiedwin36-oss <ejembiedwin36@gmail.com>
2026-06-17 01:31:06 +0100

    Update render_line.txt.go

--- render_line.txt.go
+++ render_line.txt.go
@@ -2,34 +2,25 @@ package main
 
 import "testing"
 
-// func TestRenderLine(t *testing.T) {
-// 	//1. load the banner map
-// 	bannerMap, err := readBannerFile("standard")
-// 	if err != nil {
-// 		t.Fatal("could not read banner file")
-// 	}
-
-// 	//2.  call renderLine with a space
-
-// 	result := renderLine(" ", bannerMap)
-
-// 	// 3. check the result is not empty
-// 	if result == "" {
-// 		t.Errorf("expectet output, got empty string")
-// 	}
-// }
-
 func TestRenderLine(t *testing.T) {
-	bannerMap, err := readBannerFile("standard")
-	if err != nil {
-		t.Fatal("could not read banner file")
+    bannerMap, err := ReadBannerFile("standard")
+    if err != nil {
+        t.Fatal("could not read banner file")
+    }
+    result := RenderLine(" ", bannerMap)
+    if result == "" {
+        t.Errorf("expected output, got empty string")
+    }
+}
 
-	}
-
-	expected := "           \n    /\\     \n   /  \\    \n  / /\\ \\   \n / ____ \\  \n/_/    \\_\\ \n           \n           \n"
-	result := renderLine("A", bannerMap)
-
-	if result != expected {
-		t.Errorf("got:\n%s\nwant\n%s", result, expected)
-	}
+func TestRenderLineA(t *testing.T) {
+    bannerMap, err := ReadBannerFile("standard")
+    if err != nil {
+        t.Fatal("could not read banner file")
+    }
+    expected := "           \n    /\\     \n   /  \\    \n  / /\\ \\   \n / ____ \\  \n/_/    \\_\\ \n           \n           \n"
+    result := RenderLine("A", bannerMap)
+    if result != expected {
+        t.Errorf("got:\n%s\nwant:\n%s", result, expected)
+    }
 }
