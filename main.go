1403e4248facafb68395e881781c789bccc56159
ejembiedwin36-oss <ejembiedwin36@gmail.com>
2026-06-17 01:32:42 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,32 +6,33 @@ import (
 	"strings"
 )
 
-func main() {
 
+func main() {
 	var input, banner string
-	if len(os.Args) < 2 {
+	if len(os.Args)< 2 {
 		fmt.Println("Usage: go run . \"Hello\"")
 		return
-	} else if len(os.Args) == 2 {
+	}else if len(os.Args)== 2 {
 		input = os.Args[1]
 		banner = "standard"
-	} else if len(os.Args) == 3 {
+	}else if len(os.Args)== 3 {
 		input = os.Args[1]
 		banner = os.Args[2]
 	}
 
-	bannerMap, err := readBannerFile(banner)
+	bannermap, err := ReadBannerFile(banner)
 	if err != nil {
-		fmt.Println("Error: invalid banner name")
+		fmt.Println("Error:", err)
 		return
 	}
-	inputLines := strings.Split(input, "\\n")
-	for _, line := range inputLines {
+
+	inputlines := strings.Split(input, "\\n")
+	for _, line := range inputlines {
 		if line == "" {
 			fmt.Println()
 			continue
 		}
-		fmt.Print(renderLine(line, bannerMap))
-		fmt.Println()
+		fmt.Print(RenderLine(line, bannermap))
 	}
+
 }
