ca3c489887971a4bd29c6ba2f118848fa82ddc6c
ejembiedwin36-oss <ejembiedwin36@gmail.com>
2026-06-17 01:28:51 +0100

    Update read_banner.txt.go

--- read_banner.txt.go
+++ read_banner.txt.go
@@ -2,22 +2,53 @@ package main
 
 import "testing"
 
+func TestReadBannerFileInvalid(t *testing.T) {
+    _, err := ReadBannerFile("wrongname")
+    if err == nil {
+        t.Errorf("expected error for invalid banner name, got nil")
+    }
+}
 
-// func TeastReadBannerFileInvalid(t *testing.T) {
-// 	_, err := readBannerFile("wrongname")
-// 	if err != nil {
-// 		t.Errorf("expected error for invalid banner name, got nil")
-// 	}
-// }
+func TestReadBannerFileValid(t *testing.T) {
+    bannerMap, err := ReadBannerFile("standard")
+    if err != nil {
+        t.Errorf("expected no error, got %v", err)
+    }
+    if len(bannerMap) == 0 {
+        t.Errorf("expected banner map to have entries, got empty map")
+    }
+}
 
-func TeastReadBannerFileInvalid(t *testing.T) {
-	bannerMap, err := readBannerFile("standard")
-	if err != nil {
-		t.Errorf("expected no error, got %v", err)
+func TestReadBannerFileMapSize(t *testing.T) {
+    bannerMap, err := ReadBannerFile("standard")
+    if err != nil {
+        t.Fatal("could not read banner file")
+    }
+    if len(bannerMap) != 95 {
+        t.Errorf("expected 95 characters, got %d", len(bannerMap))
+    }
+}
 
-	}
-	if len(bannerMap) == 0 {
-		t.Errorf("expected banner map to have entries, got map")
-	}
-	
+func TestSpaceCharacterHas8Lines(t *testing.T) {
+    bannerMap, err := ReadBannerFile("standard")
+    if err != nil {
+        t.Fatal("could not read banner file")
+    }
+    if len(bannerMap[' ']) != 8 {
+        t.Errorf("expected 8 lines, got %d", len(bannerMap[' ']))
+    }
+}
+
+func TestReadBannerFileShadow(t *testing.T) {
+    _, err := ReadBannerFile("shadow")
+    if err != nil {
+        t.Errorf("expected no error for shadow banner, got %v", err)
+    }
+}
+
+func TestReadBannerFileThinkertoy(t *testing.T) {
+    _, err := ReadBannerFile("thinkertoy")
+    if err != nil {
+        t.Errorf("expected no error for thinkertoy banner, got %v", err)
+    }
 }
