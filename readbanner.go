4d280288094385b2a00b7489925aebbddca3169b
ejembiedwin36-oss <ejembiedwin36@gmail.com>
2026-06-17 01:27:13 +0100

    Update readbanner.go

--- readbanner.go
+++ readbanner.go
@@ -1,16 +1,6 @@
 package main
 
 import (
-	"os"
-	"strings"
-)
-
-func readBannerFile(banner string) (map[rune][]string, error) {
-	filename := "banner-files/" + banner + ".txt"
-	data, err := os.ReadFile(filename)
-	if err !=package main
-
-import (
 	"fmt"
 	"os"
 	"strings"
@@ -41,20 +31,4 @@ func ReadBannerFile(banner string) (map[rune][]string, error) {
 		bannermap[char] = lines[start : start+8]
 	}
 	return bannermap, nil
-}
- nil {
-		return nil, err
-	}
-	content := string(data)
-	content = strings.ReplaceAll(content, "\r\n", "\n")
-	lines := strings.Split(content, "\n")
-
-	bannermap := make(map[rune][]string)
-	for i := 0; i < 95; i++ {
-		char := rune(32 + i)
-		start := 1 + i*9
-		bannermap[char] = lines[start : start+8]
-	}
-	return bannermap, nil
-
 }
