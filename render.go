eb5d1f08e722ccaee4991002a0712291db093083
ejembiedwin36-oss <ejembiedwin36@gmail.com>
2026-06-17 01:29:40 +0100

    Update render.go

--- render.go
+++ render.go
@@ -1,12 +1,17 @@
-package main
+package main  
 
-func renderLine(line string, bannermap map[rune][]string) string {
+
+func RenderLine(line string, bannermap map[rune][]string) string {
 	result := ""
+
 	for i := 0; i < 8; i++ {
 		for _, ch := range line {
+			if ch < 32 || ch > 126 {
+				return "unformatted string"
+			}
 			result += bannermap[ch][i]
 		}
 		result += "\n"
 	}
-	return result
+	return  result
 }
